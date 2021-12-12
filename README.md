28d911a3744acd46a644730978d6d4a93bd33dc3
Ayako Woollan <ayako.woollan@gmail.com>
2021-12-12 11:52:59 +1100

    upddating readme, script, html

--- README.md
+++ README.md
@@ -1,8 +1,23 @@
 ## About
-
-
+```
+This is a timed quiz on JavaScript fundamentals that stores high scores so that the user can gauge their progress compared to their peers.
+This app runs in the browser and features dynamically updated HTML and CSS powered by JavaScript code.
+```
 
 ## Instruction
+```
+When the user click the start button 
+a timer starts and I am presented with a question.
+When they answer a question,
+another question will be presented.
+WHEN the user answer a question incorrectly,
+time will be subtracted from the clock.
+When all questions are answered or the timer reaches 0,
+the game is over.
+When the game is over,
+the user can save their initials and their score.
+
+```
 
 
 
---
+++ assets/script.js
@@ -0,0 +1,65 @@
+var quizContainer = document.getElementById('#quiz')
+var answerBtnContainer = document.getElementById('#answers');
+var resultsContainer = document.getElementById('#results');
+
+
+const Question1 {
+    question: "";
+answers: {
+    1:
+    2:
+    3:
+    4:
+}
+correctAnswer: "2"
+};
+
+const Question2 {
+    question: "";
+answers: {
+    1:
+    2:
+    3:
+    4:
+}
+correctAnswer: "2"
+};
+
+const Question3 {
+    question: "";
+answers: {
+    1:
+    2:
+    3:
+    4:
+}
+correctAnswer: "2"
+};
+
+const Question4 {
+    question: "";
+answers: {
+    1:
+    2:
+    3:
+    4:
+}
+correctAnswer: "2"
+};
+
+
+
+
+function Timer() {
+    if (isWin) {
+
+    }
+}
+
+function generateQuiz() {
+
+}
+
+function showResults() {
+
+}
