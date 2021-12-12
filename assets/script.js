e12e8bd0f7772d63d1534b5e692cb72a6208147f
Ayako Woollan <ayako.woollan@gmail.com>
2021-12-12 22:01:21 +1100

    updating css javascript html

--- assets/script.js
+++ assets/script.js
@@ -1,68 +1,58 @@
+// variables
 var startBtn = document.getElementById('startBtn');
 
+var container = document.getElementById('container')
 var quizContainer = document.getElementById('quiz')
-var answerBtnContainer = document.getElementById('answers');
+var answerBtnContainer = document.getElementById('btns');
 var resultsContainer = document.getElementById('results');
 var timer = document.getElementById('timer')
 
-var timeCounter = 60;
+var currentIndex = '0';
+
+// timer
+var timeCounter = 61;
 timer.style.display = 'none';
+// isWin
+var isWin = false;
 
 
-var startQuiz = startBtn.addEventListener('click', generateQuiz());
+// functions
+var startQuiz = startBtn.addEventListener('click', function () {
+    startTimer();
+    clearScreen();
+    generateQuiz();
+});
 
-const question1 = {
-    question: "A term used by developers that means to improve the way your code is written while still making sure it performs the same action.",
-    answers: {
-        1: '1: refactor',
-        2: '2: loop',
-        3: '3: pattern matching',
-        4: '4: debug',
-    },
-    correctAnswer: "1"
-};
-
-const question2 = {
-    question: "What is a JavaScript element that represents either TRUE or FALSE values?",
-    answers: {
-        1: '1: Function',
-        2: '2: Event',
-        3: '3: Boolean',
-        4: '4: Argument',
-    },
-    correctAnswer: "3"
-};
-
-const question3 = {
-    question: "A placeholder for a piece of information that can change.",
-    answers: {
-        1: '1: event',
-        2: '2: variable',
-        3: '3: command',
-        4: '4: function',
-    },
-    correctAnswer: "2"
-};
-
-const question4 = {
-    question: "Putting commands in correct order so computers can read the commands.",
-    answers: {
-        1: '1: event',
-        2: '2: conditionals',
-        3: '3: variable',
-        4: '4: sequencing',
-    },
-    correctAnswer: "4"
-};
+// questions
+const quiz = [{
+    question: "Q: A term used by developers that means to improve the way your code is written while still making sure it performs the same action.",
+    choices: ['1: refactor', '2: loop', '3: pattern matching', '4: debug'],
+    answer: "1: refactor"
+},
+{
+    question: "Q: What is a JavaScript element that represents either TRUE or FALSE values?",
+    choices: ['1: function', '2: event', '3: boolean', '4: argument'],
+    answer: '3: Boolean'
+},
+{
+    question: "Q: A placeholder for a piece of information that can change.",
+    choices: ['1: event', '2: variable', '3: command', '4: function'],
+    answer: '2: variable'
+},
+{
+    question: "Q: Putting commands in correct order so computers can read the commands.",
+    choices: ['1: conditionals', '2: event', '3: variable', '4: sequencing'],
+    answer: '4: sequencing'
+},]
 
 
-
+// timer
 function startTimer() {
 
     var timerInterval = setInterval(function () {
         timer.style.display = 'block';
         timeCounter--;
-        timer.textContent = `Time: ${timeCounter}`;
+        timer.textContent = `Time: ${timeCounter} sec`;
 
         if (isWin) {
             clearInterval(timerInterval);
@@ -77,11 +67,33 @@ function startTimer() {
 
 }
 
+
+function clearScreen() {
+    quizContainer.innerText = ' ';
+    answerBtnContainer.innerText = ' ';
+    resultsContainer.innerText = ' ';
+}
 function generateQuiz() {
+    currentIndex++;
+    for (let i = 0; i < quiz[currentIndex].choices.length; i++) {
+
+        quizContainer.innerText = quiz[currentIndex].question;
+        var button = document.createElement('button');
+        console.log(button)
+        button.setAttribute('class', 'btn')
+        button.innerHTML = quiz[currentIndex].choices[i];
+        answerBtnContainer.appendChild(button);
+    }
+}
+
+function addElements() {
 
 }
 
 function showResults() {
+    if (isWin) {
+
+    }
 
 }
 
@@ -94,5 +106,7 @@ function showWin() {
 }
 
 function gameOver() {
+    clearScreen();
+
 
 }
