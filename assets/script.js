512a7c4485451566416d578958b0f2b495b43b91
Ayako Woollan <ayako.woollan@gmail.com>
2021-12-12 13:52:52 +1100

    adding questions to javascript

--- assets/script.js
+++ assets/script.js
@@ -4,48 +4,50 @@ var resultsContainer = document.getElementById('#results');
 
 
 const question1 = {
-    question: "",
+    question: "A term used by developers that means to improve the way your code is written while still making sure it performs the same action.",
     answers: {
-        1: '',
-        2: '',
-        3: '',
-        4: '',
+        1: '1: refactor',
+        2: '2: loop',
+        3: '3: pattern matching',
+        4: '4: debug',
     },
-    correctAnswer: "2"
+    correctAnswer: "1"
 };
 
 const question2 = {
     question: "What is a JavaScript element that represents either TRUE or FALSE values?",
     answers: {
-        1: 'Boolean',
-        2: 'Event',
-        3: 'Function',
-        4: 'Argument',
-    },
-    correctAnswer: "2"
-};
-
-const question3 = {
-    question: "",
-    answers: {
-        1: 'Conditional Argument',
-        2: '',
-        3: '',
-        4: '',
+        1: '1: Function',
+        2: '2: Event',
+        3: '3: Boolean',
+        4: '4: Argument',
     },
     correctAnswer: "3"
 };
 
-const question4 = {
-    question: "";
+const question3 = {
+    question: "A placeholder for a piece of information that can change.",
     answers: {
-        1: 'Conditional Argument',
-        2: '',
-        3: '',
-        4: '',
+        1: 'event',
+        2: 'variable',
+        3: 'command',
+        4: 'function',
+    },
+    correctAnswer: "2"
+};
+
+const question4 = {
+    question: "Putting commands in correct order so computers can read the commands.";
+    answers: {
+        1: 'event',
+        2: 'conditionals',
+        3: 'variable',
+        4: 'sequencing',
     },
     correctAnswer: "4"
 };
+
+
 
 
 
