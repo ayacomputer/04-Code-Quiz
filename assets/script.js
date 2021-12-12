19e879801e80a981ace099a4403684bea926dc7f
Ayako Woollan <ayako.woollan@gmail.com>
2021-12-12 13:15:38 +1100

    updating javascript

--- assets/script.js
+++ assets/script.js
@@ -3,48 +3,48 @@ var answerBtnContainer = document.getElementById('#answers');
 var resultsContainer = document.getElementById('#results');
 
 
-const Question1 {
-    question: "";
-answers: {
-    1:
-    2:
-    3:
-    4:
-}
-correctAnswer: "2"
+const question1 = {
+    question: "",
+    answers: {
+        1: '',
+        2: '',
+        3: '',
+        4: '',
+    },
+    correctAnswer: "2"
 };
 
-const Question2 {
-    question: "";
-answers: {
-    1:
-    2:
-    3:
-    4:
-}
-correctAnswer: "2"
+const question2 = {
+    question: "What is a JavaScript element that represents either TRUE or FALSE values?",
+    answers: {
+        1: 'Boolean',
+        2: 'Event',
+        3: 'Function',
+        4: 'Argument',
+    },
+    correctAnswer: "2"
 };
 
-const Question3 {
-    question: "";
-answers: {
-    1:
-    2:
-    3:
-    4:
-}
-correctAnswer: "2"
+const question3 = {
+    question: "",
+    answers: {
+        1: 'Conditional Argument',
+        2: '',
+        3: '',
+        4: '',
+    },
+    correctAnswer: "3"
 };
 
-const Question4 {
+const question4 = {
     question: "";
-answers: {
-    1:
-    2:
-    3:
-    4:
-}
-correctAnswer: "2"
+    answers: {
+        1: 'Conditional Argument',
+        2: '',
+        3: '',
+        4: '',
+    },
+    correctAnswer: "4"
 };
 
 
