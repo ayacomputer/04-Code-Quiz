40467957a1c734a2521cf7c34f63e6f29e6be0f4
Ayako Woollan <ayako.woollan@gmail.com>
2021-12-13 14:46:55 +1100

    updating Javascript

--- assets/script.js
+++ assets/script.js
@@ -7,7 +7,7 @@ var answerBtnContainer = document.getElementById('btns');
 var resultContainer = document.getElementById('result');
 var timer = document.getElementById('timer')
 
-var currentIndex = '0';
+var currentIndex = -1;
 
 // timer
 var timeCounter = 61;
@@ -27,22 +27,22 @@ var startQuiz = startBtn.addEventListener('click', function () {
 const quiz = [{
     question: "Q: A term used by developers that means to improve the way your code is written while still making sure it performs the same action.",
     choices: ['1: refactor', '2: loop', '3: pattern matching', '4: debug'],
-    answer: "btn0"
+    answer: '1: refactor'
 },
 {
     question: "Q: What is a JavaScript element that represents either TRUE or FALSE values?",
     choices: ['1: function', '2: event', '3: boolean', '4: argument'],
-    answer: 'btn2'
+    answer: '3: boolean'
 },
 {
     question: "Q: A placeholder for a piece of information that can change.",
     choices: ['1: event', '2: variable', '3: command', '4: function'],
-    answer: 'btn1'
+    answer: '2: variable'
 },
 {
     question: "Q: Putting commands in correct order so computers can read the commands.",
     choices: ['1: conditionals', '2: event', '3: variable', '4: sequencing'],
-    answer: 'btn3'
+    answer: '4: sequencing'
 },]
 
 
@@ -74,8 +74,8 @@ function clearScreen() {
     resultContainer.innerText = ' ';
 }
 function generateQuiz() {
+    currentIndex++;
     for (let i = 0; i < quiz[currentIndex].choices.length; i++) {
-
         quizContainer.innerText = quiz[currentIndex].question;
         var button = document.createElement('button');
         console.log(button)
@@ -87,10 +87,16 @@ function generateQuiz() {
 
     btn0.addEventListener('click', function () {
         console.log('working');
+        console.log(currentIndex);
         resultContainer.innerText = '';
-        if (btn0 === quiz[currentIndex].answer) {
+        if (btn0.innerText === quiz[currentIndex].answer) {
             showCorrect();
-            generateQuiz();
+            if (currentIndex === quiz.length) {
+                showWin();
+            } else {
+                clearScreen();
+                generateQuiz();
+            }
         }
         else {
             showWrong();
@@ -100,9 +106,14 @@ function generateQuiz() {
     btn1.addEventListener('click', function () {
         console.log('working');
         resultContainer.innerText = '';
-        if (btn1 == quiz[currentIndex].answer) {
+        if (btn1.innerText == quiz[currentIndex].answer) {
             showCorrect();
-            generateQuiz();
+            if (currentIndex === quiz.length) {
+                showWin();
+            } else {
+                clearScreen();
+                generateQuiz();
+            }
         }
         else {
             showWrong();
@@ -111,9 +122,14 @@ function generateQuiz() {
     btn2.addEventListener('click', function () {
         console.log('working');
         resultContainer.innerText = '';
-        if (btn2 == quiz[currentIndex].answer) {
+        if (btn2.innerText == quiz[currentIndex].answer) {
             showCorrect();
-            generateQuiz();
+            if (currentIndex === quiz.length) {
+                showWin();
+            } else {
+                clearScreen();
+                generateQuiz();
+            }
         }
         else {
             showWrong();
@@ -123,21 +139,14 @@ function generateQuiz() {
     btn3.addEventListener('click', function () {
         console.log('working');
         resultContainer.innerText = '';
-        if (btn3 == quiz[currentIndex].answer) {
+        if (btn3.innerText == quiz[currentIndex].answer) {
             showCorrect();
-            generateQuiz();
-        }
-        else {
-            showWrong();
-        }
-    })
-
-    btn4.addEventListener('click', function () {
-        console.log('working');
-        resultContainer.innerText = '';
-        if (btn4 == quiz[currentIndex].answer) {
-            showCorrect();
-            generateQuiz();
+            if (currentIndex === quiz.length) {
+                showWin();
+            } else {
+                clearScreen();
+                generateQuiz();
+            }
         }
         else {
             showWrong();
@@ -145,7 +154,7 @@ function generateQuiz() {
     })
 
 
-    currentIndex++;
+
 
 }
 
@@ -158,8 +167,13 @@ function showCorrect() {
 
 function showWrong() {
     resultContainer.innerText = `Wrong`;
-    timeCounter -= 10;
+    if (timeCounter >= 10) {
+        timeCounter -= 10;
+    } else {
+        gameOver();
+    }
 }
+
 
 
 
@@ -185,10 +199,10 @@ function displayInput() {
 
 }
 
-function storeData() {
+// function storeData() {
 
 
-}
+// }
 
 function gameOver() {
     clearScreen();
