af3521fa2f96363e45f22acd788a4eafad33dc4f
Ayako Woollan <ayako.woollan@gmail.com>
2021-12-13 13:26:15 +1100

    updating javascript

--- assets/script.js
+++ assets/script.js
@@ -27,22 +27,22 @@ var startQuiz = startBtn.addEventListener('click', function () {
 const quiz = [{
     question: "Q: A term used by developers that means to improve the way your code is written while still making sure it performs the same action.",
     choices: ['1: refactor', '2: loop', '3: pattern matching', '4: debug'],
-    answer: "1: refactor"
+    answer: "btn0"
 },
 {
     question: "Q: What is a JavaScript element that represents either TRUE or FALSE values?",
     choices: ['1: function', '2: event', '3: boolean', '4: argument'],
-    answer: '3: Boolean'
+    answer: 'btn2'
 },
 {
     question: "Q: A placeholder for a piece of information that can change.",
     choices: ['1: event', '2: variable', '3: command', '4: function'],
-    answer: '2: variable'
+    answer: 'btn1'
 },
 {
     question: "Q: Putting commands in correct order so computers can read the commands.",
     choices: ['1: conditionals', '2: event', '3: variable', '4: sequencing'],
-    answer: '4: sequencing'
+    answer: 'btn3'
 },]
 
 
@@ -74,32 +74,94 @@ function clearScreen() {
     resultContainer.innerText = ' ';
 }
 function generateQuiz() {
-    currentIndex++;
     for (let i = 0; i < quiz[currentIndex].choices.length; i++) {
 
         quizContainer.innerText = quiz[currentIndex].question;
         var button = document.createElement('button');
         console.log(button)
+        button.setAttribute('id', `btn${i}`)
         button.setAttribute('class', 'btn')
         button.innerHTML = quiz[currentIndex].choices[i];
         answerBtnContainer.appendChild(button);
     }
 
-    if ( === ) {
+    btn0.addEventListener('click', function () {
+        console.log('working');
+        resultContainer.innerText = '';
+        if (btn0 === quiz[currentIndex].answer) {
+            showCorrect();
+            generateQuiz();
+        }
+        else {
+            showWrong();
+        }
+    })
 
-    } else {
-        resultContainer.innerText = "Wrong";
-    }
+    btn1.addEventListener('click', function () {
+        console.log('working');
+        resultContainer.innerText = '';
+        if (btn1 == quiz[currentIndex].answer) {
+            showCorrect();
+            generateQuiz();
+        }
+        else {
+            showWrong();
+        }
+    })
+    btn2.addEventListener('click', function () {
+        console.log('working');
+        resultContainer.innerText = '';
+        if (btn2 == quiz[currentIndex].answer) {
+            showCorrect();
+            generateQuiz();
+        }
+        else {
+            showWrong();
+        }
+    })
+
+    btn3.addEventListener('click', function () {
+        console.log('working');
+        resultContainer.innerText = '';
+        if (btn3 == quiz[currentIndex].answer) {
+            showCorrect();
+            generateQuiz();
+        }
+        else {
+            showWrong();
+        }
+    })
+
+    btn4.addEventListener('click', function () {
+        console.log('working');
+        resultContainer.innerText = '';
+        if (btn4 == quiz[currentIndex].answer) {
+            showCorrect();
+            generateQuiz();
+        }
+        else {
+            showWrong();
+        }
+    })
+
+
+    currentIndex++;
+
 }
 
 
 
-function showResults() {
-    if (isWin) {
-
-    }
+function showCorrect() {
+    resultContainer.innerText = `Correct`;
 
 }
+
+function showWrong() {
+    resultContainer.innerText = `Wrong`;
+    timeCounter -= 10;
+}
+
+
 
 function isWin() {
     isWin = true;
@@ -131,5 +193,5 @@ function storeData() {
 function gameOver() {
     clearScreen();
     quizContainer.innerText = 'Game Over!';
-    resultContainer.innerText = `Your score is ${timeCounter}`;
+    // resultContainer.innerText = `Your score is ${timeCounter}`;
 }
